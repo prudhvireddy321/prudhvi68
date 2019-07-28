90d8db5be76cfe279a13824729f39da60f4c47b6
prudhvireddy321 <40226166+prudhvireddy321@users.noreply.github.com>
2019-07-28 16:22:24 +0530

    Update and rename hj.py to hk.py

--- hj.py
+++
@@ -1,9 +0,0 @@
-import sys, string, math
-s = int(input())
-L = list(map(int,input().split()))
-L2 = []
-for i in range(s) :
-    if L[i] == i : L2.append(i)
-L3 = sorted(L2)
-if len(L3) == 0 : print(-1)
-else :     print(*L3)
