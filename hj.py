e5a01055aac843eade1cc7e8f234906c36dfb5c1
prudhvireddy321 <40226166+prudhvireddy321@users.noreply.github.com>
2019-07-28 16:17:27 +0530

    Create hj.py

---
+++ hj.py
@@ -0,0 +1,9 @@
+import sys, string, math
+n = int(input())
+L = list(map(int,input().split()))
+L2 = []
+for i in range(n) :
+    if L[i] == i : L2.append(i)
+L3 = sorted(L2)
+if len(L3) == 0 : print(-1)
+else :     print(*L3
